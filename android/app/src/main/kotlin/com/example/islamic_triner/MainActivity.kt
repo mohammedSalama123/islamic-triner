456fc7f7b4564a872b0bcb64609284222a80ac2c
mohamedSalama123 <mohamed3salama33@gmail.com>
2021-10-24 13:16:06 +0200

    initial project add empty home screen

---
+++ android/app/src/main/kotlin/com/example/islamic_triner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.islamic_triner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routName = 'HomeScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
